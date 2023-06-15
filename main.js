27d398d374fc5518a59193f220e22d86b49b0b88
Lakshmiallanka <durgaallanka@gmail.com>
2023-06-15 12:10:41 +0530

    hii

---
+++ main.js
@@ -0,0 +1,124 @@
+//var x=10;
+//console.log(x);
+//var x=20;
+
+//document.getElementById("demo").innerHTML=x;
+//let y=30;
+//console.log(y); 
+//var firstName="web";
+//var lastName="development"; 
+//console.log(firstName);
+//console.log(lastName);
+//var firstName="web";
+//var lastName="development";
+//document.getElementById("demo1").innerHTML=firstName;
+//document.querySelector(".para1").innerHTML=lastName;
+
+//let a="web";
+//let b="development";
+//console.log(a);
+//console.log(b);
+
+//var A="makers";
+//var B="mind";
+//document.getElementById("demo1").innerHTML=A;
+//document.querySelector(".para1").innerHTML=B;
+
+
+// window alert//
+//var firstName="submitted successfully";
+//window.alert(firstName);
+// window.confirm(firstName);
+//window.prompt(firstName);
+// airthemetic operators//
+//let p=15;
+//let q=20;
+//let r=p+q;
+//console.log(r);
+//document.getElementById("demo2").innerHTML=r;
+
+//let s=p-q;
+//let t=p*q;
+//let u=p/q;
+//console.log(s);
+//console.log(t);
+//console.log(u);
+//document.getElementById("demo3").innerHTML=s;
+//document.getElementById("demo4").innerHTML=t;
+//document.getElementById("demo5").innerHTML=u;
+
+//assignment operators//
+//var i=5;
+//i +=9;
+//console.log(i);
+//document.querySelector(".para2").innerHTML=i;
+
+//var i=5;
+//i -=9;
+//console.log(i);
+//document.querySelector(".para3").innerHTML=i;
+
+//comparision operators//
+//const z=10;
+//const o=20;
+//console.log(z>o);
+//console.log(z<o);
+//console.log(z<=o);
+//console.log(z>=o);
+//console.log(z==o);
+//console.log(z===o);
+
+//logical operator//
+// AND operator
+//var g=40;
+//var h=40;
+//console.log(g>h&&g<h);
+//console.log(g<h&&g>h);
+//console.log(g<h&&g<h);
+//console.log(g>h&&g>h);
+
+
+//NOT operator//
+//let j=10;
+//let k=20;
+//console.log((!j<k)); 
+ //console.log((!j>k));
+
+
+ // functions//
+ function myFunction(x,y)
+ {
+    return console.log(x+y);
+ }
+myFunction(3,4); 
+
+const g=40;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
